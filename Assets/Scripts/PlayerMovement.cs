9588babcbd056ce05115a21fefc2e888e0feba86
Elkin Franco <RetroElkinTG@gmail.com>
2024-02-10 22:04:53 +1100

    Update player and basic movement.
    The pixel sizes are fixed and basic 4-way movement is added to the gameplay loop.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 myPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        myPosition = Vector3.zero;
+        myPosition.x = Input.GetAxisRaw("Horizontal");
+        myPosition.y = Input.GetAxisRaw("Vertical");
+        if (myPosition != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+    }
+
+    // Move the character
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + myPosition * speed * Time.deltaTime);
     }
 }
